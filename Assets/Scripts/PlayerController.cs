44938bf3db1c162e6c12598fd167936958ff8dcf
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2021-11-05 19:03:26 +0000

    Implemented boost mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,38 @@ public class PlayerController : MonoBehaviour
 {
 Rigidbody2D rb2D;
 [SerializeField] float spinSpeed;
+[SerializeField] float boostSpeed;
+[SerializeField] float baseSpeed;
+SurfaceEffector2D surfaceEffector2D;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
     {
+        SpinPlayer();
+        RespondToBoost();
+    }
+
+    void SpinPlayer()
+    {
         float spinAmount = spinSpeed * Input.GetAxis("Horizontal");
         rb2D.AddTorque(-spinAmount * Time.deltaTime);
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+
+        else 
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
     }
 }
